772fba902eff92ab35dff2282f13f14b128d77c2
17167 <17167@burnside.school.nz>
2020-05-22 09:25:58 +1200

    login working yeet

--- Project.py
+++ Project.py
@@ -4,25 +4,66 @@
 
 from flask import Flask, render_template, redirect, url_for, request, session, flash, abort
 from flask_login import LoginManager, UserMixin
+from functools import wraps
 import sqlite3
 import os
  
 app = Flask(__name__)
-login = LoginManager(app)
 
-#if no users are logged in, refers user to login page
-@app.route('/git pu')
+#login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
+
+#index page
+@app.route('/')
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route('/home')
 def home():
-    if not session.get('logged in'):
-        return render_template('login.html')
+    return "yo"
 
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True 
-    else:
-        flash('Incorrect Credentials')
-    return home()
+#login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['password'] != 'password' or request.form['username'] != 'admin':
+            error = "Invalid Credentials. Try Again."
+        else:
+            session['logged_in'] = True
+            flash("You're logged in as admin")
+            return redirect(url_for('account')) 
+    return render_template('login.html', error=error)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/account')
+def account():
+    if request.method == "GET":
+        if 'logged_in' in session:
+            return render_template('account.html')
+        else:
+            return redirect(url_for('login'))
+    return render_template('account.html')
+
+#logs user out, redirects them to login page
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out!')
+    return redirect(url_for('home'))
 
 #incase of error, enters debugging mode
 if __name__ == '__main__':
